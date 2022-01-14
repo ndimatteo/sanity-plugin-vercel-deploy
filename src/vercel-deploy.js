5c1b90e52ffb36debc177c59633e9219e153331a
Nick DiMatteo <hello@nickdimatteo.com>
2022-01-14 14:19:50 -0500

    fix team api request

--- src/vercel-deploy.js
+++ src/vercel-deploy.js
@@ -59,7 +59,7 @@ const VercelDeploy = () => {
     if (pendingDeploy.team) {
       try {
         const fetchTeam = await axios.get(
-          `https://api.vercel.com/v2/teams/${pendingDeploy.team}`,
+          `https://api.vercel.com/v2/teams?slug=${pendingDeploy.team}`,
           {
             headers: {
               Authorization: `Bearer ${pendingDeploy.token}`
